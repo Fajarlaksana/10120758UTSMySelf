2fac652b9aeb6bd8cb204835b0cd71017cbdd562
fajarlaksana <fajarlaksana13@gmail.com>
2023-06-10 13:08:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a10120758utsmyself/OnBoardingAdapter.java
@@ -0,0 +1,60 @@
+package com.example.a10120758utsmyself;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class OnBoardingAdapter extends  RecyclerView.Adapter<OnBoardingAdapter.OnBoardingViewHolder> {
+    //(27 Mei 2023, 10120758, Fajar Laksana, IF-9)
+
+    private List<com.example.a10120758utsmyself.OnBoardingItem> onBoardingItemList;
+
+    public OnBoardingAdapter(List<com.example.a10120758utsmyself.OnBoardingItem> onBoardingItemList) {
+        this.onBoardingItemList = onBoardingItemList;
+    }
+
+    @NonNull
+    @Override
+    public OnBoardingViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new OnBoardingViewHolder(
+                LayoutInflater.from(parent.getContext()).inflate(
+                        R.layout.item_container_onboarding, parent, false
+                )
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull OnBoardingViewHolder holder, int position) {
+        holder.setOnBoardingData(onBoardingItemList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return onBoardingItemList.size();
+    }
+
+    class OnBoardingViewHolder extends RecyclerView.ViewHolder{
+        private TextView textTittle, textDescription;
+        private ImageView imageOnBoarding;
+
+        OnBoardingViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textTittle = itemView.findViewById(R.id.textTittle);
+            textDescription = itemView.findViewById(R.id.textDesc);
+            imageOnBoarding = itemView.findViewById(R.id.imageOnBoarding);
+        }
+
+        void setOnBoardingData(com.example.a10120758utsmyself.OnBoardingItem onBoardingItem){
+            textTittle.setText(onBoardingItem.getTittle());
+            textDescription.setText(onBoardingItem.getDescription());
+            imageOnBoarding.setImageResource(onBoardingItem.getImage());
+        }
+    }
+}
---
+++ app/src/main/java/com/example/a10120758utsmyself/OnBoardingItem.java
@@ -0,0 +1,32 @@
+package com.example.a10120758utsmyself;
+
+public class OnBoardingItem {
+    //(27 Mei 2023, 10120758, Fajar Laksana, IF-9)
+    private int image;
+    private  String tittle;
+    private  String description;
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
